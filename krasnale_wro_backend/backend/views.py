fd68a3bd40b99b71ba2a4a3d427d3e70e460cc58
ntymczyszyn <natalia.ewa.tymczyszyn@gmail.com>
2024-05-08 22:07:36 +0200

    changed json content

--- krasnale_wro_backend/backend/views.py
+++ krasnale_wro_backend/backend/views.py
@@ -9,19 +9,19 @@ def image_upload(request):
     if request.method == 'POST':
 
         if 'image' not in request.FILES:
-            return JsonResponse({'error': 'Nie przesłano pliku'}, status=400)
+            return JsonResponse({'message': 'Nie przesłano pliku'}, status=400)
         
         uploaded_image = request.FILES['image']
         
         allowed_extensions = ['jpg', 'jpeg', 'png', 'gif']
         file_extension = uploaded_image.name.split('.')[-1].lower()
         if file_extension not in allowed_extensions:
-            return JsonResponse({'error': 'Niewłaściwe rozszerzenie pliku'}, status=400)
+            return JsonResponse({'message': 'Niewłaściwe rozszerzenie pliku'}, status=400)
         
         allowed_content_types = ['image/jpeg', 'image/png', 'image/gif']
         if uploaded_image.content_type not in allowed_content_types:
-            return JsonResponse({'error': 'Niewłaściwy typ zawartości pliku'}, status=400)
-        
-        return JsonResponse({'success': True, 'message': 'Zdjęcie przesłane pomyślnie'}, status=200)
+            return JsonResponse({'message': 'Niewłaściwy typ zawartości pliku'}, status=400)
+        print({'message': 'Zdjęcie przesłane pomyślnie'})
+        return JsonResponse({'message': 'Zdjęcie przesłane pomyślnie'}, status=200)
     else:
-        return JsonResponse({'error': 'Metoda nieobsługiwana'}, status=405)
+        return JsonResponse({'message': 'Metoda nieobsługiwana'}, status=405)
